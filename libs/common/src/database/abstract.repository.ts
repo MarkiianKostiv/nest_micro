011833075ab809bc2ad141ddee0273dabe655f5c
Markiian <markiian.kotiv@gmail.com>
2025-07-21 08:54:41 +0300

    crete common setup

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,4 +1,4 @@
-import { FilterQuery, Model, Types } from 'mongoose';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 import { AbstractDocument } from './abstract.schema';
 import { Logger, NotFoundException } from '@nestjs/common';
 
@@ -17,6 +17,42 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
   async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
     const document = await this.model
       .findOne(filterQuery)
+      .lean<TDocument>(true);
+    if (!document) {
+      this.logger.warn(`Document not found for filterQuery: ${filterQuery}`);
+      throw new NotFoundException(`Document not found`);
+    }
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, { new: true })
+      .lean<TDocument>(true);
+    if (!document) {
+      this.logger.warn(`Document not found for filterQuery: ${filterQuery}`);
+      throw new NotFoundException(`Document not found`);
+    }
+    return document;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    const document = await this.model.find(filterQuery).lean<TDocument[]>(true);
+    if (!document) {
+      this.logger.warn(`Document not found for filterQuery: ${filterQuery}`);
+      throw new NotFoundException(`Document not found`);
+    }
+    return document;
+  }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndDelete(filterQuery)
       .lean<TDocument>(true);
     if (!document) {
       this.logger.warn(`Document not found for filterQuery: ${filterQuery}`);
