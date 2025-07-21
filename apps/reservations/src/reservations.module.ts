d96837a7bbcfe4d03623df03d1bc0345d6a3c276
Markiian Kostiv <mstcr@marik>
2025-07-21 08:54:41 +0300

    crete common setup

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common/database/database.module';
+import * as Joi from 'joi';
+import { ReservationsRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+import { LoggerModule } from '@app/common/logger/logger.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: ReservationDocument.name,
+        schema: ReservationSchema,
+      },
+    ]),
+    LoggerModule,
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationsRepository],
+})
+export class ReservationsModule {}
